8502769ba0f7635a54fcad767c37ed0f5e2f8189
kimseonguk198 <ksg3941234@gmail.com>
2023-11-15 19:17:43 +0900

    crlf to lf

--- project/linux_dump.sh
+++ project/linux_dump.sh
@@ -2,7 +2,7 @@
 # MariaDB 설치
 sudo apt-get update
 1234
-sudo apt-get install -y mariadb-server
+sudo apt-get install mariadb-server
 
 # MariaDB 서비스 시작
 sudo systemctl start mariadb
@@ -14,8 +14,9 @@ create database board;
 exit
 
 
-sudo apt-get install -y mariadb-server
+sudo apt-get install git
 git clone https://github.com/kimseonguk197/mariadb_syntax.git
 cd mariadb_syntax/project
-
+sudo mysql --binary-mode -u root -p board < dumpfile.sql
+# 여기까지 잘 안되고 있음. utf-8문제 해결해야함.
 
